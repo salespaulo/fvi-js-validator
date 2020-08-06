d4671d7bab01bd34bfa1558656fb805f7997df89
Paulo Sales <salespaulo08gmail.com>
2020-08-06 19:40:28 -0300

    First validations and unit tests

--- src/index.js
+++ src/index.js
@@ -1,39 +1,62 @@
 'use strict'
 
-const doValidate = (schema, value) => {
-    const validates = [].concat(schema.validates)
-    validates.forEach(validate => {
-        const fromValue = schema.key ? (value[schema.key] ? value[schema.key] : null) : value
-        const isValid = validate(fromValue)
-        if (!isValid) {
-            throw new Error(
-                `Invalid Schema key="${schema.key}" to value="${fromValue}" with Function: ${validate}!`
-            )
+const isNull = () => value => value === null || value === undefined
+const isEmpty = () => value => isNull()(value) || value === ''
+const isNotEmpty = () => value => !isEmpty()(value)
+
+const isString = () => value => typeof value === 'string'
+const isNotString = () => value => !isString()(value)
+
+const isMin = (min = Number.MIN_SAFE_INTEGER) => value => value >= min
+const isMax = (max = Number.MAX_SAFE_INTEGER) => value => value <= max
+
+const isInteger = () => value => typeof value === 'number'
+
+const isBoolean = () => value => typeof value === 'boolean'
+
+const isObject = () => value => typeof value === 'object' && Object.keys(value).length > 0
+
+const Required = constraints => [true].concat(constraints)
+
+const Validator = schema => obj => {
+    Object.keys(schema).forEach(key => {
+        const constraints = [].concat(schema[key])
+        const isRequired = constraints[0] === true
+        const value = Object.keys(obj).find(k => k === key) && obj[key]
+        const isValueNull = isNull()(value)
+
+        if (isRequired && isValueNull) {
+            throw new Error(`Required key="${key}" value="${value}" from schema!`)
         }
+
+        if (isValueNull) {
+            return
+        }
+
+        const validates = isRequired ? constraints.slice(1) : constraints
+        validates.forEach(validate => {
+            const isValid = validate(value)
+
+            if (!isValid) {
+                throw new Error(
+                    `Invalid schema to key="${key}" from value="${value}" with validate Function: ${validate}!`
+                )
+            }
+        })
     })
 }
 
-exports.isNull = value => value === null || value === undefined
-exports.isEmpty = value => isNull(value) || value === ''
-exports.isNotEmpty = value => !isEmpty(value)
-
-exports.isString = value => typeof value === 'string'
-exports.isNotString = value => !isString(value)
-
-exports.isInteger = (min = Number.MIN_SAFE_INTEGER, max = Number.MAX_SAFE_INTEGER) => value =>
-    typeof value === 'number' && min >= min && value <= max
-exports.isNotInteger = (min, max) => value => !isNumber(min, max)(value)
-
-exports.isBoolean = value => typeof value === 'boolean'
-exports.isNotBoolean = value => !isBoolean(value)
-
-exports.isObject = value => typeof value === 'object' && Object.keys(value).length > 0
-exports.isNotObject = value => !isObject(value)
-
-exports.isOptional = (key, constraints) => value => isNull(value) || isNull(value[key]) || doValidate({ key, validates: constraints }, value)
-
-exports.Validator = constraints => value => {
-    Object.keys(constraints)
-        .map(key => ({ key, validates: constraints[key] }))
-        .forEach(schema => doValidate(schema, value))
+module.exports = {
+    Validator,
+    Required,
+    isNull,
+    isEmpty,
+    isNotEmpty,
+    isString,
+    isNotString,
+    isMin,
+    isMax,
+    isInteger,
+    isBoolean,
+    isObject,
 }
--- test/index.test.js
+++ test/index.test.js
@@ -4,6 +4,94 @@ const chai = require('chai')
 
 const app = require('../src')
 
-describe('Testing Validator JS', () => {
+const testIt = (e, containsMsg) => {
+    chai.assert.exists(e)
+    chai.assert.exists(e.message)
+    chai.assert.exists(e.message.includes)
+    chai.assert.isTrue(e.message.includes(containsMsg))
+}
 
+describe('Testing Validator JS', () => {
+    const constraintsOpts = {
+        url: app.Required([app.isString(), app.isNotEmpty()]),
+        timeout: [app.isInteger(), app.isMin(10)],
+        delay: [app.isInteger(), app.isMin(10)],
+        headers: [app.isObject()],
+        mock: [app.isBoolean()],
+    }
+    let validate = null
+
+    before(() => {
+        validate = app.Validator(constraintsOpts)
+    })
+
+    it('Init without config - FAIL', done => {
+        try {
+            validate({})
+            done('Should be thorws error!')
+        } catch (e) {
+            testIt(e, '"url"')
+            done()
+        }
+    })
+
+    it('Init with config only timeout', done => {
+        try {
+            validate({ timeout: 1000 })
+            done('Should be thorws error!')
+        } catch (e) {
+            testIt(e, '"url"')
+            done()
+        }
+    })
+
+    it('Init with config only delay', done => {
+        try {
+            validate({ delay: 10 })
+            done('Should be thorws error!')
+        } catch (e) {
+            testIt(e, '"url"')
+            done()
+        }
+    })
+
+    it('Init with config only timeout and delay', done => {
+        try {
+            validate({ timeout: 1000, delay: 10 })
+            done('Should be thorws error!')
+        } catch (e) {
+            testIt(e, '"url"')
+            done()
+        }
+    })
+
+    it('Init with config negative timeout ', done => {
+        try {
+            validate({ timeout: -10 })
+            done('Should be thorws error!')
+        } catch (e) {
+            testIt(e, '"url"')
+            done()
+        }
+    })
+
+    it('Init with config negative delay', done => {
+        try {
+            validate({ url: 'http://', timeout: 1000, delay: -10 })
+            done('Should be thorws error!')
+        } catch (e) {
+            testIt(e, '"delay"')
+            done()
+        }
+    })
+
+    it('Init with config url=google.com, timeout and delay 0', done => {
+        try {
+            validate({ url: 'http://google.com', timeout: 1 })
+            done('Should be thorws error!')
+        } catch (e) {
+            testIt(e, '"timeout"')
+            done()
+        }
+    })
 })
