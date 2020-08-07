d777141af840c1738a682cbbbb712bde34dbe5cb
Paulo Sales <salespaulo08gmail.com>
2020-08-06 22:20:47 -0300

    Includes Default func to value defaults

--- test/index.test.js
+++ test/index.test.js
@@ -8,16 +8,22 @@ const testIt = (e, containsMsg) => {
     chai.assert.exists(e)
     chai.assert.exists(e.message)
     chai.assert.exists(e.message.includes)
-    chai.assert.isTrue(e.message.includes(containsMsg))
+    try {
+        chai.assert.isTrue(e.message.includes(containsMsg))
+    } catch (error) {
+        throw e
+    }
 }
 
 describe('Testing Validator JS', () => {
     const constraintsOpts = {
         url: app.Required([app.isString(), app.isNotEmpty()]),
+        server: app.Default('server-name-default', [app.isString(), app.isNotEmpty()]),
         timeout: [app.isInteger(), app.isMin(10)],
         delay: [app.isInteger(), app.isMin(10)],
         headers: [app.isObject()],
-        mock: [app.isBoolean()],
+        mock: app.Default(false, [app.isBoolean()]),
+        age: app.Default(10, [app.isInteger(), app.isMin(10)]),
     }
     let validate = null
 
@@ -25,7 +31,19 @@ describe('Testing Validator JS', () => {
         validate = app.Validator(constraintsOpts)
     })
 
-    it('Init without config - FAIL', done => {
+    it(`OK`, done => {
+        const obj = { url: 'http://' }
+        const other = validate(obj)
+        chai.assert.exists(other.server)
+        chai.assert.equal('server-name-default', other.server)
+        chai.assert.exists(other.mock)
+        chai.assert.equal(false, other.mock)
+        chai.assert.exists(other.age)
+        chai.assert.equal(10, other.age)
+        done()
+    })
+
+    it('without config - FAIL', done => {
         try {
             validate({})
             done('Should be thorws error!')
@@ -35,7 +53,7 @@ describe('Testing Validator JS', () => {
         }
     })
 
-    it('Init with config only timeout', done => {
+    it('with config only timeout', done => {
         try {
             validate({ timeout: 1000 })
             done('Should be thorws error!')
@@ -45,7 +63,7 @@ describe('Testing Validator JS', () => {
         }
     })
 
-    it('Init with config only delay', done => {
+    it('with config only delay', done => {
         try {
             validate({ delay: 10 })
             done('Should be thorws error!')
@@ -55,7 +73,7 @@ describe('Testing Validator JS', () => {
         }
     })
 
-    it('Init with config only timeout and delay', done => {
+    it('with config only timeout and delay', done => {
         try {
             validate({ timeout: 1000, delay: 10 })
             done('Should be thorws error!')
@@ -65,7 +83,7 @@ describe('Testing Validator JS', () => {
         }
     })
 
-    it('Init with config negative timeout ', done => {
+    it('with config negative timeout ', done => {
         try {
             validate({ timeout: -10 })
             done('Should be thorws error!')
@@ -75,7 +93,7 @@ describe('Testing Validator JS', () => {
         }
     })
 
-    it('Init with config negative delay', done => {
+    it('with config negative delay', done => {
         try {
             validate({ url: 'http://', timeout: 1000, delay: -10 })
             done('Should be thorws error!')
@@ -85,7 +103,7 @@ describe('Testing Validator JS', () => {
         }
     })
 
-    it('Init with config url=google.com, timeout and delay 0', done => {
+    it('with config url=google.com, timeout and delay 0', done => {
         try {
             validate({ url: 'http://google.com', timeout: 1 })
             done('Should be thorws error!')
